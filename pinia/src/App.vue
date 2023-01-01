5ba55d668aeee1ad780f16ed61f5a381d3a1f715
aviralyadav <aviral.sit@gmail.com>
2023-01-01 20:51:11 +0530

    async actions

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -14,20 +14,23 @@
       <button @click="filter = 'all'">All Task</button>
       <button @click="filter = 'favs'">Fav Task</button>
     </nav>
+    <!-- loading -->
+    <div class="loading" v-if="loading">Tasks loading ...</div>
     <!-- Task list -->
     <div class="tasks-list" v-if="filter === 'all'">
-      <p>You have {{ taskStore.totalCount }} taks left to do</p>
-      <div v-for="task in taskStore.tasks" :key="task.id">
+      <p>You have {{ totalCount }} taks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
     <!-- fav list -->
     <div class="tasks-list" v-if="filter === 'favs'">
-      <p>You have {{ taskStore.favCount }} favs task left to do</p>
-      <div v-for="task in taskStore.favs" :key="task.id">
+      <p>You have {{ favCount }} favs task left to do</p>
+      <div v-for="task in favs" :key="task.id">
         <TaskDetails :task="task" />
       </div>
     </div>
+    <button @click="taskStore.$reset" class="reset">Reset</button>
   </main>
 </template>
 
@@ -36,12 +39,15 @@ import { ref } from '@vue/reactivity';
 import TaskDetails from './components/TaskDetails.vue';
 import TaskForm from './components/TaskForm.vue';
 import { useTaskStore } from "./store/TaskStore";
+import { storeToRefs } from 'pinia';
 export default {
   components: { TaskDetails, TaskForm },
   setup() {
     const taskStore = useTaskStore();
+    const { favCount, totalCount, tasks, loading, favs } = storeToRefs(taskStore);
+    taskStore.getTasks();
     const filter = ref('all')
-    return { taskStore, filter };
+    return { taskStore, filter, favCount, totalCount, tasks, loading, favs };
   },
 };
 </script>
