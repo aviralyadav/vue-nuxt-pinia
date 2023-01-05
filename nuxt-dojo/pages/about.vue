1e84081d268e19fa115cdbf172735c206e7560be
aviralyadav <aviral.sit@gmail.com>
2023-01-05 19:53:34 +0530

    first nuxt3 commit

---
+++ nuxt-dojo/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quae sed rem consequatur dignissimos atque, veniam, dolorum harum aliquam animi asperiores, nesciunt fugit fuga repellat doloribus quia voluptas dolorem. Molestiae!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, beatae illo. Excepturi, ipsum tempora! Enim praesentium sint repellendus quia in beatae, quod eos. Assumenda facere vitae debitis maxime in? Ratione.</p>
+    </div>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
