cd5996ed88bc2eb1fde8cc73f431354eaf68dacf
aviralyadav <aviral.sit@gmail.com>
2023-01-06 21:43:17 +0530

    data fetch error page reusable components tailwind css

--- nuxt-dojo/pages/products/index.vue
+++ nuxt-dojo/pages/products/index.vue
@@ -1,11 +1,16 @@
 <template>
-    <div>
-        <h3>Products</h3>
+    <div class="grid grid-cols-4 gap-5">
+        <div v-for="p in products" :key="p.id">
+            <ProductCard :product="p" />
+        </div>
     </div>
 </template>
 
 <script setup>
-
+definePageMeta({
+    layout: 'products'
+});
+const { data: products } = await useFetch('https://fakestoreapi.com/products');
 </script>
 
 <style scoped>
